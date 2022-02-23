4a70011725c94b916f00896f2d78386abd331ab5
williz4 <wilsonmwangeka@gmail.com>
2022-02-23 06:41:57 -0500

    Modal functionality added

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-const showModal = document.getElementById('show-modal')
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+//Show Modal, Focus on 1st input of Modal
+function showModal() {
+	modal.classList.add('show-modal');
+	websiteNameEl.focus();
+}
+
+
+//Modal event listener
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e)=> (e.target === modal ? modal.classList.remove('show-modal') : false));
